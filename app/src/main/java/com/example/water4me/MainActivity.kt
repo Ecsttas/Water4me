2aa364c2b7ab3535fe0dad74e2936dbfa769d689
Ecsttas <137838848+Ecsttas@users.noreply.github.com>
2023-07-04 00:02:02 +0200

    first

--- app/src/main/java/com/example/water4me/MainActivity.kt
+++ app/src/main/java/com/example/water4me/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.water4me
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.water4me.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bindingClass : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) //Artem, we are dolboyObu хы
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root) //Artem, we are dolboyObu хы
+
+
     }
 }
